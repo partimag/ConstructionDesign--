1ed67d94e0c9ef6b5ffeaf5df56b47470b2c62cb
Mukul Ghanghas <partimag55@gmail.com>
2023-05-18 09:58:34 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import HeroSection from "./compoents/HeroSection";
+import KeyValues from "./compoents/KeyValues";
+import Feedback from "./compoents/Feedback";
+
+function App() {
+  return (
+    <div className="font-RobotoMono">
+      <HeroSection />
+      <KeyValues />
+      <Feedback />
+    </div>
+  );
+}
+
+export default App;
